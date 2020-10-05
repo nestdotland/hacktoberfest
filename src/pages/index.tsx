00110c212f38c5c915342dfbdf3f359acb5ec8ee
John Letey <johnletey@gmail.com>
2020-10-05 22:17:06 +0100

    feat: add deno logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import registry from "../../registry.json";
 
 const Index = () => {
   console.log(registry);
+  const [isDark, setIsDark] = useState(true);
+  useEffect(() => {
+    setIsDark(
+      window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    );
+  }, []);
 
   return (
     <>
-      <h1>Coming soon ...</h1>
+      {registry.map((module) => (
+        <>
+          <h2>{module.name}</h2>
+          <img
+            src={isDark ? "/logo_dark.svg" : "/logo_light.svg"}
+            height="20px"
+          />{" "}
+          <img
+            src={isDark ? "/deno_dark.svg" : "/deno_light.svg"}
+            height="20px"
+          />
+        </>
+      ))}
     </>
   );
 };
